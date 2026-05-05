f1af248a33a746e5468a1ddb3b4ad19bd17633be
irffann-git <rxmeirfan7@gmail.com>
2026-05-05 14:56:01 +0530

    finish

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ connectDB();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:3000"],
+    origin: ["http://localhost:5173", "http://localhost:4173", "http://localhost:3000"],
     credentials: true,
   })
 );
